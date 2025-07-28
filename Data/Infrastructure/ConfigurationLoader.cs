018e2c417c351bbb03adacd80c51127efd951eb4
Andrey P <petr.and.vic@gmail.com>
2025-07-28 04:28:10 +0400

    Created the Infrastructure directory to isolate access to the database

---
+++ Data/Infrastructure/ConfigurationLoader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace Data.Infrastructure
+{
+    public static class ConfigurationLoader
+    {
+        public static IConfiguration Load()
+        {
+            return new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+        }
+    }
+}
---
+++ Data/Infrastructure/DataServiceProvider.cs
@@ -0,0 +1,40 @@
+﻿using Data.Models;
+using Data.Repositories;
+using Data.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Data.Infrastructure
+{
+    public static class DataServiceProvider
+    {
+        private static ServiceProvider? _provider;
+
+        public static void Initialize()
+        {
+            var configuration = ConfigurationLoader.Load();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            var services = new ServiceCollection();
+
+            // Подключаем DbContext с полученной строкой подключения
+            services.AddDbContext<BlazorBankContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            // Регистрируем репозитории и службы
+            services.AddScoped<IClientRepository, ClientRepository>();
+            services.AddScoped<ClientService>();
+
+            _provider = services.BuildServiceProvider();
+        }
+
+        public static T GetService<T>() where T : notnull
+        {
+            if (_provider == null)
+                throw new InvalidOperationException("DataServiceProvider is not initialized.");
+
+            return _provider.GetRequiredService<T>();
+        }
+    }
+}
