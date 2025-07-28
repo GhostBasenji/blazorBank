8955d363a2a5292ae5dda3a3637b7d2ac50aefa3
Andrey P <petr.and.vic@gmail.com>
2025-07-28 04:02:43 +0400

    I have created a Repositories directory and, for now, a temporary stub in the form of a single class.

---
+++ Data/Repositories/IClientRepository.cs
@@ -0,0 +1,36 @@
+﻿using Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repositories
+{
+    public interface IClientRepository
+    {
+        Task<IEnumerable<Client>> GetAllAsync();
+        Task<Client?> GetByIdAsync(int id);
+    }
+
+    public class ClientRepository : IClientRepository
+    {
+        private readonly BlazorBankContext _context;
+
+        public ClientRepository(BlazorBankContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Client>> GetAllAsync()
+        {
+            return await _context.Clients.ToListAsync();
+        }
+
+        public async Task<Client?> GetByIdAsync(int id)
+        {
+            return await _context.Clients.FindAsync(id);
+        }
+    }
+}
