3073f6c4e5ce08fc653a6ab11058ba6aaea081a1
Andrey P <petr.and.vic@gmail.com>
2025-08-04 23:04:07 +0400

    Renamed the Client project to ClientApp due to a conflict in file names. I also fixed the project theme display style.

---
+++ ClientApp/Program.cs
@@ -0,0 +1,45 @@
+using ClientApp.Components;
+using ClientApp.Services;
+using Data.Contexts;
+using Data.DTOs;
+using Data.Infrastructure;
+using Data.Repositories;
+using Data.Services;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<IAccountRepository, AccountRepository>();
+builder.Services.AddScoped<IAccountService, AccountService>();
+builder.Services.AddScoped<ProtectedSessionStorage>();
+builder.Services.AddScoped<CurrentClientService>();
+
+
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddDataServices(builder.Configuration);
+builder.Services.AddScoped<ClientAuthService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
---
+++ ClientApp/Services/CurrentClientService.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading.Tasks;
+using Data.Models;
+using Data.Contexts;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+
+namespace ClientApp.Services
+{
+    public class CurrentClientService
+    {
+        private readonly ProtectedSessionStorage _sessionStorage;
+        private readonly BlazorBankContext _dbContext;
+
+        public CurrentClientService(ProtectedSessionStorage sessionStorage, BlazorBankContext dbContext)
+        {
+            _sessionStorage = sessionStorage;
+            _dbContext = dbContext;
+        }
+
+        // Сохраняем ID клиента в sessionStorage (состояние в сервисе не храним)
+        public async Task SetClientAsync(Client client)
+        {
+            await _sessionStorage.SetAsync("LoggedInClientId", client.ClientId);
+        }
+
+        // Получаем клиента из sessionStorage и базы, возвращая его вызывающей стороне
+        public async Task<Client?> GetCurrentClientAsync()
+        {
+            var result = await _sessionStorage.GetAsync<int>("LoggedInClientId");
+            if (result.Success)
+            {
+                int clientId = result.Value;
+                return await _dbContext.Clients.FindAsync(clientId);
+            }
+            return null;
+        }
+
+        // Очистка sessionStorage при логауте
+        public async Task LogoutAsync()
+        {
+            await _sessionStorage.DeleteAsync("LoggedInClientId");
+        }
+    }
+}
